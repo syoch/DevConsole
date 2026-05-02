b61241b77f16d36d074d6dcf9157a224343e9f24
syoch <syoch64@gmail.com>
2026-05-02 09:09:39 +0900

    Update: Providing nix package

--- flake.nix
+++ flake.nix
@@ -8,6 +8,13 @@
       pkgs = import nixpkgs { inherit system; };
     in
     {
+      packages.x86_64-linux.default = pkgs.rustPlatform.buildRustPackage {
+        pname = "devconsole";
+        version = "1.0.1";
+
+        src = ./.;
+        cargoLock.lockFile = ./Cargo.lock;
+      };
       devShells.x86_64-linux.default = pkgs.mkShell {
         buildInputs = with pkgs; [
           cargo
